9624976178793dd7bc6048d151b47d23bae3b5f4
Sanjai <Sanjaimavi2001@gmail.com>
2026-05-23 20:29:43 +0530

    adding the tfm files

---
+++ .terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "4.85.0"
+  constraints = "~> 4.0"
+  hashes = [
+    "h1:sld/eTvevl/Af3upWX1TesnLLCCUMBQlczxo5lPzA48=",
+    "zh:17d60a6a6c1741cf1e09ac6731433a30950285eac88236e623ab4cbf23832ca3",
+    "zh:1c70254c016439dbb75cab646b4beace6ceeff117c75d81f2cc27d41c312f752",
+    "zh:35e2aa2cc7ac84ce55e05bb4de7b461b169d3582e56d3262e249ff09d64fe008",
+    "zh:417afb08d7b2744429f6b76806f4134d62b0354acf98e8a6c00de3c24f2bb6ad",
+    "zh:622165d09d21d9a922c86f1fc7177a400507f2a8c4a4513114407ae04da2dd29",
+    "zh:7cdb8e39a8ea0939558d87d2cb6caceded9e21f21003d9e9f9ce648d5db0bc3a",
+    "zh:851e737dc551d6004a860a8907fda65118fc2c7ede9fa828f7be704a2a39e68f",
+    "zh:a331ad289a02a2c4473572a573dc389be0a604cdd9e03dd8dbc10297fb14f14d",
+    "zh:b67fd531251380decd8dd1f849460d60f329f89df3d15f5815849a1dd001f430",
+    "zh:be8785957acca4f97aa3e800b313b57d1fca07788761c8867c9bc701fbe0bdb5",
+    "zh:cb6579a259fe020e1f88217d8f6937b2d5ace15b6406370977a1966eb31b1ca5",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version = "7.31.0"
+  hashes = [
+    "h1:+ZtPKLdCd+0UUg6iIIZzTQjIwyqazIjSOBbcjzdC0JU=",
+    "zh:38333f0fd70587ce67bb7e6579f5e12ced7dec3a4a3b219d69c88c7adf9d96bc",
+    "zh:54ca354fd7e40d3292efef8466a04c0e7f01ff6af8b6cfbc666ed397bdba5e95",
+    "zh:5ba0391ca807834b258e2c91b2467bf209ab41e36e31b06497b83ddf4c92b8d8",
+    "zh:6e2bc594100a759b43053393da14536e47a3a38f5bd9e87661f7bc532c420368",
+    "zh:85eea9ec2124e4b1cb08415b6bf016d837f9c52b8d543b92001875a44c36bf7e",
+    "zh:a6cf14441269cdcb99c2fddd716c3f439edb75022f603b0eda1a4b6f9562de3a",
+    "zh:ab4d6ea8d22d9ca4c06c7fff463c2ecf42caa892f6a64396b5f450031a25ca21",
+    "zh:b8c78807c5bd73c14ada286a5b6cfc47c17bd13c74f2b859f3982b31564d5f47",
+    "zh:c2c95e42e0afc4e9816f9a791b406a193dace1a679ba52489f65d5598c1fbe3a",
+    "zh:c32d39dfefe461ad52b03c5e37b4df9a2e0c2e75eed0a928a0e2fa20565b6652",
+    "zh:eba377e78aa08ea2947ea998d9f4285eda855348b15da58055cf3cf88970a5de",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
--- main.tf
+++ main.tf
@@ -1,25 +1,42 @@
-resource "google_storage_bucket" "data_bucket" {
-  name          = var.bucket_name
-  location      = var.region
-  project       = var.project_id
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  lifecycle_rule {
-    condition {
-      age = 30
-    }
-    action {
-      type = "Delete"
-    }
-  }
-}
+# resource "google_storage_bucket" "data_bucket" {
+#   name          = var.bucket_name
+#   location      = var.region
+#   project       = var.project_id
+#   force_destroy = true
+#
+#   uniform_bucket_level_access = true
+#
+#   lifecycle_rule {
+#     condition {
+#       age = 30
+#     }
+#     action {
+#       type = "Delete"
+#     }
+#   }
+# }
 
 
 resource "google_pubsub_topic" "topic" {
   name = var.topic_name
   project = var.project_id
+}
+
+resource "google_pubsub_topic" "dead_letter_topic" {
+  name = "orders-dead-letter-topic"
+  project = var.project_id
+}
+
+data "google_project" "project" {
+  project_id = "project-e85d8801-bd70-4ddd-8bc"
+}
+
+resource "google_pubsub_topic_iam_member" "dead_letter_publisher" {
+  topic = google_pubsub_topic.dead_letter_topic.name
+  role  = "roles/pubsub.publisher"
+  project = var.project_id
+
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
 
 resource "google_pubsub_subscription" "subscription" {
@@ -32,8 +49,38 @@ resource "google_pubsub_subscription" "subscription" {
   message_retention_duration = "604800s" # 7 days
 
   retain_acked_messages = true
+
+  dead_letter_policy {
+    dead_letter_topic     = google_pubsub_topic.dead_letter_topic.id
+    max_delivery_attempts = 5
+  }
+
+  retry_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
 }
 
+## the below sub
+resource "google_pubsub_subscription" "main_subscription" {
+  name  = "orders-subscription"
+  topic = google_pubsub_topic.topic.name
+  project = var.project_id
+
+  ack_deadline_seconds = 20
+
+  message_retention_duration = "604800s"
+
+  dead_letter_policy {
+    dead_letter_topic     = google_pubsub_topic.dead_letter_topic.id
+    max_delivery_attempts = 5
+  }
+
+  retry_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+}
 # resource "google_dataflow_flex_template_job" "dataflow_job" {
 #   provider = google-beta   # 👈 IMPORTANT
 #   project  = var.project_id
@@ -50,19 +97,19 @@ resource "google_pubsub_subscription" "subscription" {
 #   on_delete = "cancel"
 # }
 
-resource "google_dataflow_job" "dataflow_job" {
-  provider = google-beta   # 👈 IMPORTANT
-  project  = var.project_id
-  max_workers = 1
-  region = "us-central1"
-  name              = "pubsub-to-gcs-job"
-  template_gcs_path = "gs://dataflow-templates/latest/Cloud_PubSub_to_GCS_Text"
-
-  temp_gcs_location = "gs://${google_storage_bucket.data_bucket.name}/temp"
-
-  parameters = {
-    inputTopic           = google_pubsub_topic.topic.id
-    outputFilenamePrefix = "gs://${google_storage_bucket.data_bucket.name}/output"
-    outputDirectory      = "gs://${google_storage_bucket.data_bucket.name}/output"
-  }
-}
+# resource "google_dataflow_job" "dataflow_job" {
+#   provider = google-beta   # 👈 IMPORTANT
+#   project  = var.project_id
+#   max_workers = 1
+#   region = "us-central1"
+#   name              = "pubsub-to-gcs-job"
+#   template_gcs_path = "gs://dataflow-templates/latest/Cloud_PubSub_to_GCS_Text"
+#
+#   temp_gcs_location = "gs://${google_storage_bucket.data_bucket.name}/temp"
+#
+#   parameters = {
+#     inputTopic           = google_pubsub_topic.topic.id
+#     outputFilenamePrefix = "gs://${google_storage_bucket.data_bucket.name}/output"
+#     outputDirectory      = "gs://${google_storage_bucket.data_bucket.name}/output"
+#   }
+# }
